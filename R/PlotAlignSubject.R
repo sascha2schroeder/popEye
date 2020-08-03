8b935a1ee626dd109b46854fbff9477b584e77fc
sascha <sascha2schroeder@gmail.com>
2020-08-03 16:29:02 +0200

    PlotAlignSubject added

---
+++ R/PlotAlignSubject.R
@@ -0,0 +1,20 @@
+
+PlotAlignSubject <- function(exp, subject) {
+  
+  ntrial <- exp$out$subjects$ntrial[exp$out$subjects$subid == subject]
+  trials <- exp$out$trials$itemid[exp$out$trials$subid == subsubjectid]
+  
+  for (i in 1:ntrial) {
+    
+    line <- ReadKey()
+    
+    if (line == "q") {
+      break
+    }
+    
+    message(paste("Trial ", i, ": ", trials[i], sep = ""))
+    PlotAlign(exp, subject, i)
+    
+  }
+  
+}
