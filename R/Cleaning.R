6cf682ff0fc66798fb79da5006baef35c8b18708
Sascha Schroeder <sascha2schroeder@gmail.com>
2021-09-24 10:43:11 +0200

    Cleaning fixed

--- R/Cleaning.R
+++ R/Cleaning.R
@@ -5,7 +5,6 @@ Cleaning <- function(out, env = parent.frame(n = 2)) {
                         dur.thresh = env$exp$setup$clean$stage1Dur, 
                         dist.thresh = env$exp$setup$clean$stage1Dist)
   
-  
   out <- CleanFixations(out, 
                         dur.thresh = env$exp$setup$clean$stage2Dur,
                         dist.thres = env$exp$setup$clean$stage2Dist)
