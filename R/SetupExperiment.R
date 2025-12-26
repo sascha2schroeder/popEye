e9e3c50dd9232a46ae0890102cc703249eed9d59
Sascha Schroeder <sascha2schroeder@gmail.com>
2025-12-26 19:59:01 +0100

    Consolas 26 pt added

--- R/SetupExperiment.R
+++ R/SetupExperiment.R
@@ -547,6 +547,16 @@ SetupExperiment <- function(env = parent.frame(n = 1)) {
     font$fixed <- TRUE
   }
   
+  # Consolas, 26 pt
+  if (font$name == "Consolas" & font$size == 26) {
+    font$letpix <- data.frame(letter = " ", pixel = 20)
+    font$height <- 32
+    font$width <- 20
+    font$lead <- 8
+    font$right <- FALSE
+    font$fixed <- TRUE
+  }
+  
   # ConsolasRussian 20 pt
   if (font$name == "ConsolasRussian" & font$size == 20) {
     font$letpix <- data.frame(letter = " ", pixel = 15)
